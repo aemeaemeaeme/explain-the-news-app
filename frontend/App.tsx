94d37c9006f25b62f6ece6fe1c7874daa3eff821
Leap <git@leap.new>
2025-09-22 01:00:24 +0000

    Manual code change

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Toaster } from '@/components/ui/toaster';
 import LandingPage from './pages/LandingPage';
 import ArticlePage from './pages/ArticlePage';
 import AnalysisPage from './pages/AnalysisPage';
@@ -24,7 +23,6 @@ export default function App() {
             <Route path="/examples" element={<ExamplesPage />} />
             <Route path="/contact" element={<ContactPage />} />
           </Routes>
-          <Toaster />
         </div>
       </Router>
     </QueryClientProvider>
